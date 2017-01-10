c43fda3052396d5d32d44ea2179ba77d2d63a147
IRONBAN <heramban_minatchy@hotmail.com>
2017-01-10 11:39:30 +0100

    ajout main et footer dans index.php

--- index.php
+++ index.php
@@ -8,5 +8,16 @@
 		<h1>MON SITE GITHUB</h1>
 	</header>
 
+	<main>
+		<section>
+			<h2>Ma permiere section</h2>
+		</section>
+
+	</main>
+
+	<footer>
+		<p>tous droits réservés</p>
+	</footer>
+
 </body>
 </html>
